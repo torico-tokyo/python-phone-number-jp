de4a126fa9b5dc4483a13f4c3a9682321ab62e13
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2019-02-21 14:16:36 +0900

    ハイフン/スペース入れるメソッド作った

--- phone_number_jp/__init__.py
+++ phone_number_jp/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
+from typing import Tuple
+
 from .prefix_numbers import phone_number_prefixes
 
 __author__ = 'ytyng'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __license__ = 'BSD'
 
 MAX_PREFIX_DIGIT = 5
@@ -18,7 +20,7 @@ class PhoneNumberIsNotDigits(PhoneNumberJPError):
     pass
 
 
-class UnexpectedIndex(PhoneNumberJPError):
+class InvaliDigits(PhoneNumberJPError):
     pass
 
 
@@ -26,16 +28,19 @@ class PrefixNoMatch(PhoneNumberJPError):
     pass
 
 
-def _split_tail(prefix, tail):
+def _split_tail(prefix: str, tail: str) -> Tuple[str, str, str]:
     # 残りが7桁の場合、3, 4 で分割
     tail_split_digits = int(len(tail) / 2)
 
     return prefix, tail[:tail_split_digits], tail[tail_split_digits:]
 
 
-def split_phone_number(phone_number: str):
+def split_phone_number(phone_number: str) -> Tuple[str, str, str]:
     if not phone_number.isdigit():
         raise PhoneNumberIsNotDigits(phone_number)
+
+    if len(phone_number) < 10:
+        raise InvaliDigits(phone_number)
 
     for d in range(MAX_PREFIX_DIGIT, MIN_PREFIX_DIGIT - 1, -1):
         p = phone_number[:d]
@@ -43,3 +48,35 @@ def split_phone_number(phone_number: str):
             return _split_tail(p, phone_number[d:])
 
     raise PrefixNoMatch(phone_number)
+
+
+def split_phone_number_no_fail(phone_number: str) -> Tuple[str, str, str]:
+    """
+    無理やり3分割する。無理な時は空文字
+    """
+    try:
+        return split_phone_number(phone_number)
+    except PrefixNoMatch:
+        return _split_tail(phone_number[:3], phone_number[3:])
+    except InvaliDigits:
+        if len(phone_number) > 5:
+            return _split_tail(phone_number[:3], phone_number[3:])
+    return _split_tail('', phone_number)
+
+
+def insert_hyphens(phone_number: str) -> str:
+    """
+    Insert hyphens.
+    """
+    if '-' in phone_number:
+        return phone_number
+    return '-'.join(split_phone_number_no_fail(phone_number))
+
+
+def insert_spaces(phone_number: str) -> str:
+    """
+    Insert spaces.
+    """
+    if ' ' in phone_number:
+        return phone_number
+    return ' '.join(split_phone_number_no_fail(phone_number))
--- tests.py
+++ tests.py
@@ -3,9 +3,11 @@
 import unittest
 
 try:
-    from .phone_number_jp import split_phone_number
+    from .phone_number_jp import split_phone_number, PhoneNumberIsNotDigits, \
+        PrefixNoMatch, InvaliDigits, insert_hyphens, insert_spaces
 except ImportError:
-    from phone_number_jp import split_phone_number
+    from phone_number_jp import split_phone_number, PhoneNumberIsNotDigits, \
+        PrefixNoMatch, InvaliDigits, insert_hyphens, insert_spaces
 
 test_numbers = [
     ('08011112222', [3, 4, 4]),
@@ -35,6 +37,36 @@ class PhoneNumberJPTest(unittest.TestCase):
                 segment2 = phone_number[start:start + digit]
                 self.assertEqual(segment, segment2)
                 start += digit
+
+    def test_no_number(self):
+        with self.assertRaises(PhoneNumberIsNotDigits):
+            split_phone_number('0901111222a')
+
+    def test_no_match(self):
+        with self.assertRaises(PrefixNoMatch):
+            split_phone_number('09111112222')
+        with self.assertRaises(PrefixNoMatch):
+            split_phone_number('10011112222')
+
+    def test_invalid_digits(self):
+        with self.assertRaises(InvaliDigits):
+            split_phone_number('091112222')
+
+    def test_insert_hyphens(self):
+        self.assertEqual(
+            insert_hyphens('0311112222'), '03-1111-2222')
+        self.assertEqual(
+            insert_hyphens('09011112222'), '090-1111-2222')
+        self.assertEqual(
+            insert_hyphens('090-1111-2222'), '090-1111-2222')
+
+    def test_insert_spaces(self):
+        self.assertEqual(
+            insert_spaces('0158611222'), '01586 11 222')
+        self.assertEqual(
+            insert_spaces('0158111222'), '0158 111 222')
+        self.assertEqual(
+            insert_spaces('01586 11 222'), '01586 11 222')
 
 
 if __name__ == '__main__':
